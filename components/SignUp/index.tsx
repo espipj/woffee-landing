fea9a70f7a8a3cd32cc2fe6a895febfb9f312f4b
Pablo Espinosa <espipj@gmail.com>
2021-12-26 23:57:21 +0100

    Added SignUp Component

---
+++ components/SignUp/index.tsx
@@ -0,0 +1,15 @@
+const SignUp = () => {
+  return (
+    <>
+      <p className="font-sans font-semibold text-xl antialiased">
+        Sign-Up for product updates in the future
+      </p>
+      <div className="rounded-lg shadow-md py-9 px-4">
+        <input type="email"></input>
+        <button>Sign-Up</button>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
