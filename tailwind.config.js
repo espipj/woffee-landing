1796e29219defffa9ea98a8463cc355de03c6b6e
Pablo Espinosa <espipj@gmail.com>
2021-12-27 18:12:32 +0100

    Added woffee colours and fonts to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: { extend: { backdropBlur: { xs: "3px" } } },
+  theme: {
+    extend: {
+      backdropBlur: { xs: "3px" },
+      colors: { "woffee-1": "#3794FC" },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
   plugins: [],
 };
