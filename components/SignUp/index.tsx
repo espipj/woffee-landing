88c3f6e44333c8e2021fb5cebbcbc0e82d0e1c6c
Pablo Espinosa <espipj@gmail.com>
2022-01-09 00:18:59 +0100

    Styled sign up form

--- components/SignUp/index.tsx
+++ components/SignUp/index.tsx
@@ -1,14 +1,15 @@
 const SignUp = () => {
   return (
-    <>
-      <p className="font-sans font-semibold text-xl antialiased">
-        Sign-Up for product updates in the future
-      </p>
-      <div className="rounded-lg shadow-md py-9 px-4">
-        <input type="email"></input>
-        <button>Sign-Up</button>
-      </div>
-    </>
+    <div className="flex space-x-3">
+      <input
+        type="email"
+        className="basis-2/3 border border-slate-800 rounded-full px-3 font-sans antialiased"
+        placeholder="Email"
+      ></input>
+      <button className="basis-1/3 px-4 py-2 font-semibold text-sm bg-woffee-1 hover:bg-woffee-1/90 text-white rounded-full shadow-sm">
+        Sign-Up
+      </button>
+    </div>
   );
 };
 
