63add3e1a60b336a67a3020d521b55767aa5112f
espipj <espipj@gmail.com>
2022-01-08 23:38:31 +0000

    Deploying to gh-pages from @ espipj/woffee-landing@5cf7536f2ae41010298371d771e193d0612e1af4 🚀

--- _next/static/z4eWF-2MbAYm5PidqcUoj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f1949f5f32762b03.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/download":["static/chunks/pages/download-dcd9da3857233e87.js"],sortedPages:["/","/_app","/_error","/download"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
