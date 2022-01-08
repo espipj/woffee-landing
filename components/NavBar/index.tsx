5cf7536f2ae41010298371d771e193d0612e1af4
Pablo Espinosa <espipj@gmail.com>
2022-01-09 00:37:36 +0100

    Full width navbar

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <div className="sticky md:top-2 md:mx-9 top-1 mx-1 my-1 z-40 backdrop-blur-xs bg-white/90 rounded-2xl max-w-fit">
+    <div className="sticky md:top-2 md:mx-9 top-1 mx-1 my-1 z-40 backdrop-blur-xs bg-white/90 rounded-2xl ">
       <div className="max-w-8xl mx-auto">
         <div className="md:pt-2 md:pb-1 md:px-4 pt-2 pb-1 px-3">
           <img src="/woffee.svg" alt="Woffee Logo" width={180} height={40} />
