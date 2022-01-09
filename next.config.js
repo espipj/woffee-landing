3f81d2925de95bbcb8471f57275a6867cff822b5
Pablo Espinosa <espipj@gmail.com>
2022-01-09 01:06:38 +0100

    Fixed assets url on deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix: "https://espipj.github.io/woffee-landing",
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,5 +13,4 @@ module.exports = {
     },
   },
   plugins: [],
-  assetPrefix: "/woffee-landing/",
 };
