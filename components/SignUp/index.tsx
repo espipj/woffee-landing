98f9290fb921bfbb7ad429437e588c994b1b3931
Pablo Espinosa <espipj@gmail.com>
2022-01-09 13:17:15 +0100

    Fixed sign up form

--- components/SignUp/index.tsx
+++ components/SignUp/index.tsx
@@ -1,12 +1,12 @@
 const SignUp = () => {
   return (
-    <div className="flex space-x-3">
+    <div className="flex space-x-1 sm:space-x-3">
       <input
         type="email"
-        className="basis-2/3 border border-slate-800 rounded-full px-3 font-sans antialiased"
+        className="w-0 flex-1 sm:basis-3/4 border border-slate-800 rounded-full px-3 font-sans text-lg"
         placeholder="Email"
       ></input>
-      <button className="basis-1/3 px-4 py-2 font-semibold text-sm bg-woffee-1 hover:bg-woffee-1/90 text-white rounded-full shadow-sm">
+      <button className="min-w-fit flex-1 sm:basis-1/4 font-sans font-semibold  text-lg text-white antialiased bg-woffee-1 hover:bg-woffee-1/90 px-2 sm:px-3 py-1 rounded-full shadow-sm">
         Sign-Up
       </button>
     </div>
