929960231cd6d36a4df5dc9e179d5e3d1f8be22b
Pablo Espinosa <espipj@gmail.com>
2022-04-15 13:06:11 +0200

    quick fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,7 +54,7 @@ const Home: NextPage = () => {
                 Some Heading
               </h2>
               <p className="font-sans text-xl antialiased">
-                Some description: ndustry. Lorem Ipsum has been the industry's
+                Some description: ndustry. Lorem Ipsum has been the industrys
                 standard dummy text ever since the 1500s, when an unknown
                 printer took a galley of type and scrambled it to make a type
                 specimen book. It has survived not only five centuries, but also
@@ -69,7 +69,7 @@ const Home: NextPage = () => {
                 Some Heading
               </h2>
               <p className="font-sans text-xl antialiased">
-                Some description: ndustry. Lorem Ipsum has been the industry's
+                Some description: ndustry. Lorem Ipsum has been the industrys
                 standard dummy text ever since the 1500s, when an unknown
                 printer took a galley of type and scrambled it to make a type
                 specimen book. It has survived not only five centuries, but also
